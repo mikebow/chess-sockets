70271ce0817bc1ead926e3afa783f5a3011811e7
Mike Bowersock <mikebow@umich.edu>
2024-03-26 14:58:23 -0400

    added client-side logic to handle the communcation message received from the sockets server including only prompting a player when it is their turn and having them wait otherwise.

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const URL = "ws://localhost:443";
 const connection = new WebSocket(URL);
 
 
-
-
 let movePrompt = [
     {
         type: 'input',
@@ -19,68 +17,67 @@ let movePrompt = [
 
 
 
-const mainLoop = async (fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") => {
-    const chess = new Chess(fen);
+
+let chess = new Chess();
+const makeMove = async (move=null, fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" /*TODO: add option for a starting position*/ ) => {
+
     try {
-        while (!chess.isGameOver()) {
-            console.log(chess.ascii());
-            
-            let { moveMade } = await inquirer.prompt(movePrompt);
-            console.log(moveMade);
-            chess.move(moveMade);
+        if (move) chess.move(move); // There will not a be a move when the game starts.
+        console.log(chess.ascii());
         
-        }
-        console.log(chess.ascii());
-        console.log("Game over!");
-    }
-    
-
-    catch (e) {
-
-        console.error("Invalid move");
-        mainLoop(chess.fen());
-    }
-};
-
-
-mainLoop();
-
-
-
-const makeMove = async (fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") => {
-    const chess = new Chess(fen);
-
-    try {
-        console.log(chess.ascii());
         let { moveMade } = await inquirer.prompt(movePrompt);
-        console.log(moveMade);
         chess.move(moveMade);
+        console.log(chess.ascii());
         return chess;
     }
 
     catch (e) {
-        console.error(error);
-        makeMove(chess.fen());
+        console.error(e);
+        chess = new Chess();
     }
 }
+
+connection.on('open', () => {
+    console.log("Successfully connected to chess-sockets!")
+})
+
+
 
 // TODO: review all bases and logic to handle an ongoing game
 connection.on('message', async (message) => {
     try {
         const data = JSON.parse(message);
 
+        // Not waiting (meaning it is your move) and there is a game in progress
         if (!data.waiting && data.gameInProgress) {
-            console.log(data.ascii)
-            // TODO send the data to the ws server
-            const resultingPosition = await makeMove(data.fen);
+            console.log(data.message);
+            const resultingPosition = await makeMove(data.move);
+            const moveData = {
+                move: resultingPosition.history()[resultingPosition.history().length - 1]
+            }
+            const jsonData = JSON.stringify(moveData);
+            connection.send(jsonData)
         }
 
-        else if (data.waiting) {
-            console.log("Waiting for opponent to join...");
+        else {
+            console.log(data.message);
         }
+
     }
     catch (e) {
         console.error(e)
     }
     
+})
+
+
+connection.on('error', (error) => {
+    console.error('WebSocket connection error:', error.code);
+    console.log("Did you start the ws server?\n");
+    console.log("Try the following: \n\tcd ws-server\n\tnode index.js");
+});
+
+connection.on('close', () => {
+    console.log("\nServer disconnecting now...");
+    process.exit();
 })
